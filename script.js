be8e7b87bfbb84c37db20c63b5d59c51464f7b4f
Eran Shiber <44651526+EranShiber@users.noreply.github.com>
2019-12-27 20:31:24 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
     const boxAera = document.querySelector('.box-aera');
     boxAera.addEventListener("mouseover", (x) => {
+        document.querySelector(".v").style = `
+        animation: vMove ease-in-out 1s infinite;
+        `
     let newInterval = setInterval(() => {
+
         if(boxAera.childElementCount < 500){ 
-            let randomValues = Math.random(Math.floor()) * 10;
+            let randomValues = Math.random(Math.floor()) * 15;
             let newBox = document.createElement('li');
             newBox.className = "newBox";
              newBox.style = `
-             left: ${Math.round(Math.random(Math.floor()) * 95)}%;
-             animation: boxMove ${randomValues} ease-in infinite reverse;
-             width: 5px;
-             height: ${randomValues * Math.round(Math.random(Math.floor()) )}px;
+             left: ${Math.round(Math.random(Math.floor()) * 98)}%;
+             animation: boxMove ${randomValues}s ease-in-out infinite reverse;
+             width: ${Math.random(Math.floor()) * 7}px;
+             height: ${Math.random(Math.floor()) * 7}px;
+         
              `
              boxAera.appendChild(newBox)
-    
-
             
         } else {
 
